570351866bed279c283eb0e1a32fb87a893937bc
Jorge Luis Grullón Marroquín <jgrullon@popoyan.com>
2024-01-03 15:42:57 -0600

    validation roles in graphql

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,12 +1,13 @@
 const boom = require("@hapi/boom");
+const checkRolesGql = require("./../utils/checkRolesGql");
+const checkJwtGql = require("./../utils/checkJwtGql");
+
 const CategoryService = require("./../services/category.service");
 const service = new CategoryService();
 
 const addCategory = async (_, { dto }, context) => {
-  const { user } = await context.authenticate("jwt", { session: false });
-  if (!user) {
-    throw boom.unauthorized("Inicia sesion o registrate, no tienes acceso!");
-  }
+  const user = await checkJwtGql(context);
+  checkRolesGql(user, "admin");
   return service.create(dto);
 };
 
